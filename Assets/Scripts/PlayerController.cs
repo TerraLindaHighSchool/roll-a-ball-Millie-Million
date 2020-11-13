1fc1fe360f48342c0683f626dd1ff289bd3a63fe
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-11-13 15:03:53 -0800

    Tutorial 7.3 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
+    public TextMeshProUGUI countText;
 
     private Rigidbody rb;
     private int count;
@@ -17,6 +19,8 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+
+        SetCountText();
     }
 
     private void OnMovementAction(InputValue movementValue)
@@ -24,6 +28,11 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 
     private void FixedUpdate()
@@ -38,6 +47,8 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count = count + 1;
+
+            SetCountText();
         }
     }
 }
